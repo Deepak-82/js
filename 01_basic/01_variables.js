3108e0209a93f4f8adfb709647d047023ffd547d
Deepak-82 <dpal489591@gmail.com>
2026-01-10 11:47:19 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "deepak@google.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
+let accountstate;
+
+// accountId = 2
+accountEmail = "dp@dp.com"
+accountPassword = "6789"
+accountcity = "Baglore"
+
+console.log(accountId);
+
+
+console.table( [accountId, accountEmail, accountPassword, accountcity, accountstate] )
+
